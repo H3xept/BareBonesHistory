7dacca1c445908ab5b31400bc23f9094775986d1
Rory Brown <rorybrown660@gmail.com>
2019-02-15 20:37:07 +0000

    updated/added function declarations

--- src/history.h
+++ src/history.h
@@ -4,16 +4,18 @@
 #include <stdio.h>
 
 // General
-void import_history_from_file(FILE* file);
-void export_history_to_file(FILE* file);
-void clear(void);
+void init_history();
+void import_history_from_file(char* filename);
+void export_history_to_file(char* filename);
+void clear_history(void);
 char** get_entire_history(void);
 
 // Entries
-char* current_entry(void);
-char* get_entry(unsigned int index);
-char* latest_entry_with_prefix(char* prefix);
-char* latest_entry_containing(char* substring);
+void add_history_entry(char* line);
+char* current_history_entry(void);
+char* get_history_entry(unsigned int index);
+char* latest_history_entry_with_prefix(char* prefix);
+char* latest_history_entry_containing(char* substring);
 unsigned int entries_n(void);
 
 #endif
